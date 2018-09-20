4bf68e56591b7f5c91c4feb90b3254189467dfc5
pedronl <ikbenpedroh@gmail.com>
2018-09-20 13:51:48 +0200

    removed geolocation

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
-var options = {
-  enableHighAccuracy: true,
-  timeout: 4000,
-  maximumAge: 0
-};
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 4000,
+//   maximumAge: 0
+// };
 
-function success(pos) {
-  var crd = pos.coords;
-  var lat = crd.latitude
-  var lon = crd.longitude
-
-
-  map = L.map('mapDiv').setView([lat, lon], 13);
-  // set map tiles source
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-  }).addTo(map);
-  // add marker to the map
-  marker = L.marker([lat, lon]).addTo(map);
-  // add popup to the marker
-  marker.bindPopup("<b>MOBIQUITY</b><br />TOMMASSOALIBOSTRAAT <br />").openPopup();
+// function success(pos) {
+//   var crd = pos.coords;
+//   var lat = crd.latitude
+//   var lon = crd.longitude
 
 
+//   map = L.map('mapDiv').setView([lat, lon], 13);
+//   // set map tiles source
+//   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+//     maxZoom: 18,
+//   }).addTo(map);
+//   // add marker to the map
+//   marker = L.marker([lat, lon]).addTo(map);
+//   // add popup to the marker
+//   marker.bindPopup("<b>MOBIQUITY</b><br />TOMMASSOALIBOSTRAAT <br />").openPopup();
 
-  console.log('Your current position is:');
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
 
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+//   console.log('Your current position is:');
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude: ${crd.longitude}`);
+//   console.log(`More or less ${crd.accuracy} meters.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error, options);
